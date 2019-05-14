c5a270622b2cfe4c71e457012e779d6e586b3835
aryalsunil555 <aryalsunil555@gmail.com>
2019-05-14 11:58:02 +0545

    search

--- app/src/main/java/com/api/EmployeeAPI.java
+++ app/src/main/java/com/api/EmployeeAPI.java
@@ -5,10 +5,16 @@ import java.util.List;
 import model.Employee;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface EmployeeAPI {
     @GET("employees")
     Call<List<Employee>> getEmployee();
 
+    //get employee on the basis of empoyee id
+@GET("employee/{empID}")
+
+
+    Call<Employee> getEmployeeByID(@Path("empID") int empID);
 }
 
